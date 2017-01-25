cdf117b87d71ffed10063080b84a32e81b183d3e
Dustin R. Heart <dustin.welden@avalara.com>
2017-01-24 22:45:23 -0800

    Moved relevant code into project, including tests. ChannelSpec test copied from amqp-scala-client project until its use in RpcSpec can be refactored out

---
+++ src/test/scala/space/spacelift/amqp/Test2.scala
@@ -0,0 +1,36 @@
+package space.spacelift.amqp
+
+import scala.concurrent.ExecutionContext
+import akka.actor.ActorSystem
+import akka.pattern.ask
+import akka.util.Timeout
+import space.spacelift.amqp.RpcClient.Request
+import space.spacelift.amqp.Amqp.Publish
+import scala.concurrent.duration._
+import com.rabbitmq.client.ConnectionFactory
+
+/**
+ * Created by fabrice on 31/12/13.
+ */
+object Test2 extends App {
+  import ExecutionContext.Implicits.global
+
+  implicit val system = ActorSystem("mySystem")
+  implicit val timeout: Timeout = 1 second
+  // create an AMQP connection
+  val conn = system.actorOf(ConnectionOwner.props(new ConnectionFactory(), reconnectionDelay = 5 seconds), "connection")
+
+  val client = ConnectionOwner.createChildActor(conn, RpcClient.props(), Some("RpcClient"))
+
+  // send 1 request every second
+  while(true) {
+    println("sending request")
+    (client ? Request(Publish("amq.direct", "my_key", "test".getBytes("UTF-8")))).mapTo[RpcClient.Response].map(response => {
+      // we expect 1 delivery
+      val delivery = response.deliveries.head
+      println("reponse : " + new String(delivery.body))
+    })
+    Thread.sleep(1000)
+  }
+
+}
